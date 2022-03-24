875eebaa490a15b8df90abd4cd668c03c26bd367
imre-gabor <gabor@aut.bme.hu>
2022-03-24 11:25:17 +0100

    add bank project with 2 entities

---
+++ bank/src/main/java/bank/Account.java
@@ -0,0 +1,66 @@
+package bank;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the ACCOUNT database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Account.findAll", query="SELECT a FROM Account a")
+public class Account implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int accountid;
+
+	private double balance;
+
+	@Temporal(TemporalType.DATE)
+	private Date createdate;
+
+	//bi-directional many-to-one association to Client
+	@ManyToOne
+	@JoinColumn(name="CLIENTID")
+	private Client client;
+
+	public Account() {
+	}
+
+	public int getAccountid() {
+		return this.accountid;
+	}
+
+	public void setAccountid(int accountid) {
+		this.accountid = accountid;
+	}
+
+	public double getBalance() {
+		return this.balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public Date getCreatedate() {
+		return this.createdate;
+	}
+
+	public void setCreatedate(Date createdate) {
+		this.createdate = createdate;
+	}
+
+	public Client getClient() {
+		return this.client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+}
---
+++ bank/src/main/java/bank/Client.java
@@ -0,0 +1,78 @@
+package bank;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the CLIENT database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Client.findAll", query="SELECT c FROM Client c")
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int clientid;
+
+	private String address;
+
+	private String name;
+
+	//bi-directional many-to-one association to Account
+	@OneToMany(mappedBy="client")
+	private List<Account> accounts;
+
+	public Client() {
+	}
+
+	public int getClientid() {
+		return this.clientid;
+	}
+
+	public void setClientid(int clientid) {
+		this.clientid = clientid;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Account> getAccounts() {
+		return this.accounts;
+	}
+
+	public void setAccounts(List<Account> accounts) {
+		this.accounts = accounts;
+	}
+
+	public Account addAccount(Account account) {
+		getAccounts().add(account);
+		account.setClient(this);
+
+		return account;
+	}
+
+	public Account removeAccount(Account account) {
+		getAccounts().remove(account);
+		account.setClient(null);
+
+		return account;
+	}
+
+}
