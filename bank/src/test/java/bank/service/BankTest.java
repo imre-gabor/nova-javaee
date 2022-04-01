efae81085769ca8aeb8f128fb3c408a1aa8a9eba
imre-gabor <gabor@aut.bme.hu>
2022-04-01 09:15:22 +0200

    add test case

--- bank/src/test/java/bank/service/BankTest.java
+++ bank/src/test/java/bank/service/BankTest.java
@@ -2,6 +2,7 @@ package bank.service;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import javax.ejb.EJB;
@@ -11,13 +12,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import bank.dao.AccountDao;
 import bank.dao.ClientDao;
 import bank.model.Account;
+import bank.model.Client;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BankTest {
@@ -31,11 +32,6 @@ public class BankTest {
 	@Mock
 	AccountDao accountDao;
 	
-//	@Before
-//	public void init() {
-//		MockitoAnnotations.initMocks(bank);
-//	}
-//	
 	
 	//@Test(expected = BankException.class) // --> nem tudjuk ellenőrizni a kivétel részleteit
 	@Test
@@ -43,13 +39,15 @@ public class BankTest {
 		
 		//ARRANGE
 		int clientId = 100;
-		Mockito.when(clientDao.findById(100)).thenReturn(Optional.empty());
+		when(clientDao.findById(100)).thenReturn(Optional.empty());
 		
 		//ACT
 		try {
 			bank.createAccountForClient(new Account(), clientId);
+			//ASSERT
 			fail("Expected exception not thrown.");
 		} catch (Exception e) {
+			//ASSERT
 			if(! (e instanceof BankException) ) {
 				e.printStackTrace();
 				fail("Unexpected exception " + e.getMessage());
@@ -57,8 +55,33 @@ public class BankTest {
 				assertEquals("Client with given id does not exist", e.getMessage());
 			}
 		}
+	}
+	
+	@Test
+	public void testThatAccountForExistingClientIsCreated() throws Exception {
+		//ARRANGE
+		Client client = new Client();
+		int clientid = 1;
+		client.setClientid(clientid);
+		client.setAccounts(new ArrayList<Account>());
+		when(clientDao.findById(clientid)).thenReturn(Optional.of(client));
+		
+		int accountid = 2;
+		Account account = new Account();
+		double balance = 100.0;
+		account.setBalance(balance);
+		account.setAccountid(accountid);
+		
+		//ACT
+		bank.createAccountForClient(account, clientid);
 		
 		//ASSERT
+		assertEquals(balance, account.getBalance(), 0.000001);
+		assertEquals(clientid, account.getClient().getClientid());
+		assertEquals(accountid, client.getAccounts().get(0).getAccountid());
+		//dátumot is ellenőrizni lehetne
+		
+		verify(accountDao, times(1)).create(account);
 		
 	}
 }
